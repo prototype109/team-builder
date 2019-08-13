bb3580b7598f2876ca16c1f02a92369873addf5c
Brandon Nichols <brandonaronnichols@gamil.com>
2019-08-12 23:33:52 -0500

    Added names for inputs and hmtlFor for labels as well as values to input

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form(){
+    const [member, setMember] = useState({name: '', email: '', role: ''});
+
     return(
         <form>
-            <label>
+            <label htmlFor='name'>
                 Name:
-                <input type='text'/>
+                <input type='text' name='name' value={member.name}/>
             </label>
-            <label>
+            <label htmlFor='email'>
                 email:
-                <input type='text'/>
+                <input type='text' name='email' value={member.email}/>
             </label>
-            <label>
+            <label htmlFor='role'>
                 role:
-                <input type='text'/>
+                <input type='text' name='role' value={member.role}/>
             </label>
         </form>
     );
