8dcd2fea2d73e45fb7c3d7cff5561112616cc44e
Brandon Nichols <brandonaronnichols@gamil.com>
2019-08-12 23:52:52 -0500

    Added handle event for onChange on input texts

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -3,19 +3,24 @@ import React, { useState } from 'react';
 function Form(){
     const [member, setMember] = useState({name: '', email: '', role: ''});
 
+    const handleChange = event => {
+        event.preventDefault();
+        setMember({...member, [event.target.name]: event.target.value});
+    };
+    
     return(
         <form>
             <label htmlFor='name'>
                 Name:
-                <input type='text' name='name' value={member.name}/>
+                <input type='text' name='name' onChange={handleChange} value={member.name}/>
             </label>
             <label htmlFor='email'>
                 email:
-                <input type='text' name='email' value={member.email}/>
+                <input type='text' name='email' onChange={handleChange} value={member.email}/>
             </label>
             <label htmlFor='role'>
                 role:
-                <input type='text' name='role' value={member.role}/>
+                <input type='text' name='role' onChange={handleChange} value={member.role}/>
             </label>
         </form>
     );
