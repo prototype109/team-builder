59538f9fc324645e61c0bab0ed29b712fb26d869
Brandon Nichols <brandonaronnichols@gamil.com>
2019-08-12 23:07:44 -0500

    Created DisplayTeam component to display team as well as made a data component that holds initial information about starting team

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import data from './data';
+import DisplayTeam from './components/DisplayTeam';
 
 function App() {
+  const [team, setTeam] = useState(data);
+
   return (
     <div className="App">
-      
+      <DisplayTeam team={team} />
     </div>
   );
 }
---
+++ team-builder/src/data.js
@@ -0,0 +1,22 @@
+const teamMembers = [
+    {
+        id: 0,
+        name: 'Ralph',
+        email: 'ralph@yahoo.com',
+        role: 'tank'
+    },
+    {
+        id: 1,
+        name: 'Jen',
+        email: 'jenny@yahoo.com',
+        role: 'attacker'
+    },
+    {
+        id: 2,
+        name: 'mimsy',
+        email: 'mim@yahoo.com',
+        role: 'attacker'
+    }
+];
+
+export default teamMembers;
